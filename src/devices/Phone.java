4b48ced7eb23eac4e3ef40cd5c82353c54ee7ee4
JanKrogul <s24534@pjwstk.edu.pl>
2022-11-13 11:34:29 +0100

    zadanie phone

--- src/devices/Phone.java
+++ src/devices/Phone.java
@@ -1,7 +1,15 @@
 package devices;
 
 public class Phone {
-
+    String tel;
+    String model;
+    String producer;
+    Phone (String tel, String model, String producer)
+    {
+        this.tel = tel;
+        this.model = model;
+        this.producer = producer;
+    }
 
 
 
